acc51410e5422f67168f4fbe345ced33ca57a6ae
Paul Tung <ptung.tw@ffn.com>
2021-03-15 11:57:30 +0800

    rewrite the timer context store

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,11 +4,11 @@ import { TimerContext } from "../store/TimerContextStore";
 import Layout from "./layout"
 
 const About: React.FC = observer(() => {
-  const timer = useContext(TimerContext)
+  const { secondsPassed } = useContext(TimerContext)
   return (
     <Layout title="TS React - About">
       <h1>About!</h1>
-      <h3>Mbox + useContext : {timer.secondsPassed}</h3>
+      <h3>Mbox + useContext : {secondsPassed}</h3>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quas ipsam, excepturi animi beatae numquam, cum ducimus asperiores quam recusandae repudiandae qui provident commodi maxime? Temporibus, nam cumque. Vel praesentium facere, eum labore neque repellendus ullam nesciunt corporis rerum amet?
       </p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,11 @@ import { TimerContext } from "../store/TimerContextStore";
 import Layout from "./layout"
 
 const HomePage: React.FC = observer(() => {
-    const timer = useContext(TimerContext)
+    const { secondsPassed } = useContext(TimerContext)
     return (
         <Layout title="TS React">
             <h1>TS React demo</h1>
-            <h3>React + TypeScript : {timer.secondsPassed}</h3>
+            <h3>React + TypeScript : {secondsPassed}</h3>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, unde. Quasi doloremque, porro doloribus eveniet ex aspernatur enim vero, nostrum expedita culpa impedit rem officiis, nisi amet nulla sed animi recusandae dolores in fugiat delectus totam veniam? Odio sunt dicta nihil, tempora mollitia dolores omnis totam fuga, quisquam quidem hic minima sequi ratione officia repudiandae qui assumenda soluta! Doloribus nam, sint dolorum ullam aut reiciendis pariatur, unde magni minima adipisci quasi possimus itaque quae eaque vitae eveniet beatae. Exercitationem veniam eius odit, hic libero dolor deleniti iste assumenda? Autem, illo.
             </p>
